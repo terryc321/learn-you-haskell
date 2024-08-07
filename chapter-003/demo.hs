438ae9e0d065aa31a2d6ca1dafc50c6040f48eb9
terry <terry.cadd@gmail.com>
2024-08-07 23:46:42 +0100

    continuing chapter 3 types and type classes

--- chapter-003/demo.hs
+++ chapter-003/demo.hs
@@ -73,16 +73,9 @@ cmp4 = "Ho Ho" == "Ho Ho"
 cmp5 = 3.432 == 3.432 
 
 {-
--- Ord is for types that have an ordering.
+Ord is for types that have an ordering.
 
 (>) :: Ord a => a -> a -> Bool
-
-editor
-if region is single line --
-otherwise use block comment
-
-
-nested block comments ?
 
 -}
 
@@ -92,17 +85,6 @@ cmp7 = "Abrakadabra" `compare` "Zebra"
 cmp8 = 5 >= 2
 cmp9 = 5 `compare` 3  
 
-show 3
-
--- this is a single line comment 
-
-{- 
-this is a line
-this is alline
-this is a line
--}
-
--- hoogle haskell google ?
 
 {- 
 Members of Show can be presented as strings.
@@ -111,10 +93,134 @@ The most used function that deals with the Show typeclass is show.
 It takes a value whose type is a member of Show and presents it to us as a string.
 -}
 
+s1 = show 3
+s2 = show 5.334
+s3 = show True
+
 {- 
-undo redo
-where is hydra also ... how do we config that spicy nut sack
-do we have haskell lsp for emacs installation ?
+Read is sort of the opposite typeclass of Show.
+The read function takes a string and returns a type which is a member of Read.
+-}
+
+r1 = read "True" || False
+r2 = read "8.2" + 3.8
+r3 = read "5" - 2
+r4 = read "[1,2,3,4]" ++ [3]  
+
+{- 
+read "4"
+*** Exception: Prelude.read: no parse
+-}
+r5 = read "4" :: Int
+r6 = read "4" :: Integer
+r7 = read "4" :: Float
+r8 = read "4" :: Double
+
+{- 
+
+:t show
+show :: Show a => a -> String
+
+:t read
+read :: Read a => String -> a
+-}
+
+
+-- we can help haskell infer the type by telling it explicitly 
+r10 = read "5" :: Int
+r11 = read "5" :: Float
+r12 = (read "5" :: Float) * 4
+r13 = read "[1,2,3,4]" :: [Int]
+r14 = read "(3, 'a')" :: (Int, Char)
+
+{- 
+Enum members are sequentially ordered types — they can be enumerated.
+The main advantage of the Enum typeclass is that we can use its types in list ranges.
+They also have defined successors and predecesors, which you can get with the succ
+and pred functions.
+Types in this class: (), Bool, Char, Ordering, Int, Integer, Float and Double.
+
+can i list all types that belong to class Enum ?
+-}
+
+e1 = ['a'..'e']
+e2 = [LT .. GT]
+e3 = [3 .. 5]
+e4 = succ 'B'
+
+-- Bounded members have an upper and a lower bound.
+-- b for bounds
+b1 = minBound :: Int  
+b2 = maxBound :: Char
+b3 = maxBound :: Bool
+b4 = minBound :: Bool
+
+{-
+once we get used to getting types of things using :t , we will just
+list the types of procedures we use 
+
+minBound :: Bounded a => a
+minBound :: Bounded a => a
+
+Num is a numeric typeclass.
+Its members have the property of being able to act like numbers.
+Let's examine the type of a number.
+
+:t 20
+20 :: Num a => a
+
+It appears that whole numbers are also polymorphic constants.
+They can act like any type that's a member of the Num typeclass.
+
+
+-}
+
+c1 = 20 :: Int
+c2 = 20 :: Float
+c3 = 20 :: Double
+c4 = 20 :: Integer
+
+{- 
+common math operators
+Num class things act like numbers
+Fractional class
+Integral class
+
+(*) :: Num a => a -> a -> a
+(+) :: Num a => a -> a -> a
+(-) :: Num a => a -> a -> a
+(/) :: Fractional a => a -> a -> a
+(div) :: Integral a => a -> a -> a
+
+To join Num, a type must already be friends with Show and Eq.
+
+Num includes all numbers, including real numbers and integral numbers,
+
+Integral is also a numeric typeclass.
+Integral includes only integral (whole) numbers.
+In this typeclass are Int and Integer.
+
+Floating includes only floating point numbers, so Float and Double.
+
+A very useful function for dealing with numbers is fromIntegral.
+It has a type declaration of fromIntegral :: (Num b, Integral a) => a -> b.
+From its type signature we see that it takes an integral number and turns it into a more
+general number.
+
+That's useful when you want integral and floating point types to work together nicely.
+For instance, the length function has a type declaration of length :: [a] -> Int instead
+of having a more general type of (Num b) => length :: [a] -> b.
+
+I think that's there for historical reasons or something, although in my opinion, it's pretty stupid.
+Anyway, if we try to get a length of a list and then add it to 3.2, we'll get an error
+because we tried to add together an Int and a floating point number.
+
+So to get around this, we do fromIntegral (length [1,2,3,4]) + 3.2 and it all works out.
+
+Notice that fromIntegral has several class constraints in its type signature.
+That's completely valid and as you can see, the class constraints are separated by
+commas inside the parentheses.
+
 -}
 
 
