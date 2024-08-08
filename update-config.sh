91aad3cd05b9d58c0f785a31e9279c001ca7e2df
terry <terry.cadd@gmail.com>
2024-08-08 01:06:53 +0100

    added a config update for emacs

--- update-config.sh
+++ update-config.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+# change directory from wherever we were
 cd ~/code/learn-you-haskell/
 echo `pwd`
 
+# save a copy of init.el
 cp -v ~/.emacs.d/init.el ~/code/learn-you-haskell/emacs-config/init.el
+
+# force this into git repo 
 git add emacs-config/init.el
 git commit -m "updated emacs haskell config"
+# push to github
 git push origin main
 
+
+#sweet!
 
 
 
